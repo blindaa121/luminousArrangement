5d8014f23d043389ceecd774ab08e6ad3fcb344d
Bryan <blindaa121@gmail.com>
2021-02-08 10:47:17 -0800

    Style info cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DonutChartContainer from './DonutChart/DonutChartContainer';
+import InfoCards from '../components/InfoCards/InfoCards'
 
 const App = () => {
   return (
     <div className="App">
       <h1>Luminous Arrangement</h1>
+      <InfoCards />
       <DonutChartContainer />
     </div>
   );
--- src/components/DonutChart/DonutChart.jsx
+++ src/components/DonutChart/DonutChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Doughnut} from 'react-chartjs-2';
+import './DonutChart.css'
 
 class DonutChart extends React.Component {
   constructor(props) {
@@ -9,14 +10,12 @@ class DonutChart extends React.Component {
       datasets: [
         {
           label: 'Rainfall',
-          backgroundColor: ['#B21F00', '#C9DE00', '#2FDE00', '#00A6B4', '#6800B4'],
-          hoverBackgroundColor: [ '#501800', '#4B5000', '#175000', '#003350', '#35014F'],
+          backgroundColor: ['#F8D525', '#386FA4', '#59A5D8', '#84D2F6', '#133C55'],
+          hoverBackgroundColor: ['#DAB707', '#184563', '#24496A', '#2B7EB6', '#184563'],
           data: [80,20,0,0,0]
         }
       ]
     }
-
-      // this.setPercentages = this.setPercentages.bind(this);
   }
 
   updateDonut() {
@@ -29,8 +28,8 @@ class DonutChart extends React.Component {
           smallCapPercentage
       } = this.props;
 
-      const bgColor = ['#B21F00', '#C9DE00', '#2FDE00', '#00A6B4', '#6800B4'];
-      const hoverColor = ['#501800', '#4B5000', '#175000', '#003350', '#35014F'];
+      const bgColor = ['#F8D525', '#386FA4', '#59A5D8', '#84D2F6', '#133C55'];
+      const hoverColor = ['#DAB707', '#184563', '#24496A', '#2B7EB6', '#184563'];
       const data = [
         bondPercentage,
         largeCapPercentage,
@@ -53,7 +52,7 @@ class DonutChart extends React.Component {
   }
   
   componentDidMount() {
-      console.log(this.props)
+    
   }
 
   componentDidUpdate(prevProps) {
@@ -63,36 +62,49 @@ class DonutChart extends React.Component {
   }
 
   render() {
+    const donutOptions = {
+      title: {
+        display: true,
+        text: 'Investment Portfolio',
+        fontSize: 20
+      },
+      legend: {
+        display: true,
+        position: 'top'
+      },
+      animation: {
+        animateScale: true,
+      },
+      tooltips: {
+        titleFontSize: 30
+      }
+    }
     return(
       <div className="">
-        <h1>Donut Chart goes here</h1>
         <div className="selection-container">
-          <button onClick={() => this.props.addRiskOne()}>1</button>
-          <button onClick={() => this.props.addRiskTwo()}>2</button>
-          <button onClick={() => this.props.addRiskThree()}>3</button>
-          <button onClick={() => this.props.addRiskFour()}>4</button>
-          <button onClick={() => this.props.addRiskFive()}>5</button>
-          <button onClick={() => this.props.addRiskSix()}>6</button>
-          <button onClick={() => this.props.addRiskSeven()}>7</button>
-          <button onClick={() => this.props.addRiskEight()}>8</button>
-          <button onClick={() => this.props.addRiskNine()}>9</button>
-          <button onClick={() => this.props.addRiskTen()}>10</button>
+          <h1>Please select a risk level for your investment portfolio</h1>
+          <div className="selection-risk">
+            <div className="selection-risk-level">
+              <span>Low</span>
+              <span>High</span>
+            </div>
+            <div className="button-container">
+              <button onClick={() => this.props.addRiskOne()}>1</button>
+              <button onClick={() => this.props.addRiskTwo()}>2</button>
+              <button onClick={() => this.props.addRiskThree()}>3</button>
+              <button onClick={() => this.props.addRiskFour()}>4</button>
+              <button onClick={() => this.props.addRiskFive()}>5</button>
+              <button onClick={() => this.props.addRiskSix()}>6</button>
+              <button onClick={() => this.props.addRiskSeven()}>7</button>
+              <button onClick={() => this.props.addRiskEight()}>8</button>
+              <button onClick={() => this.props.addRiskNine()}>9</button>
+              <button onClick={() => this.props.addRiskTen()}>10</button>
+            </div>
+          </div>
+          <Doughnut id='chart-area' data={this.state} options={donutOptions} />
         </div>
 
-        <Doughnut
-            data={this.state}
-            options={{
-                title: {
-                    display: true,
-                    text: 'Average Rainfall per month',
-                    fontSize: 20
-                },
-                legend: {
-                    display: true,
-                    position: 'right'
-                }
-            }}
-        />
+        
       </div>
     )
   }
